324b85664cf92ee7e210f269d63e7c8c6b7a43e7
Maxthon Chan <xcvista@me.com>
2014-01-29 23:25:02 +0800

    Initial Commit

---
+++ mkiptables/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mkiptables
+//
+//  Created by Maxthon Chan on 1/29/14.
+//  Copyright (c) 2014 Maxthon Chan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
